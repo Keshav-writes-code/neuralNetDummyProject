788739c1843f07f65fcb0edd500488959cfebaf6
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-08-13 23:13:44 +0530

    build: change base font size

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('daisyui'),
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+		require("daisyui"),
+		plugin(function({ addBase }) {
+			addBase({
+			   'html': { fontSize: "30px" },
+			})
+		}),
 	],
-}
+};
